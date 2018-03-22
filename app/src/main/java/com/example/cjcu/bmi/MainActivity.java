0af17df33e49a316df09efbe236c4de93b469ab3
KosJammy <G24563041@mailst.cjcu.edu.tw>
2018-03-22 15:01:09 +0800

    第二個版本(AlertDialog的寫法)

--- app/src/main/java/com/example/cjcu/bmi/MainActivity.java
+++ app/src/main/java/com/example/cjcu/bmi/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cjcu.bmi;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -37,6 +38,13 @@ public class MainActivity extends AppCompatActivity {
         float bmi = w / (h*h);
         Log.d("BMI=", String.valueOf(bmi));
         Toast.makeText(this,String.valueOf(bmi),Toast.LENGTH_LONG).show();
+
+        new AlertDialog.Builder(this)
+                .setMessage("BMI="+bmi)
+                .setTitle("BMI值")
+                .setPositiveButton("OK",null)
+                .setNegativeButton("CANCEL",null)
+                .show();
         
     }
 }
